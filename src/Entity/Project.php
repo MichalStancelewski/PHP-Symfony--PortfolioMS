2c6a43bf3931e75ad5048b4be2e803f37666dcc9
MichalStancelewski <michal@stancelewski.pl>
2021-10-09 18:46:18 +0200

    Added __toString() methods to Project and Technology

--- src/Entity/Project.php
+++ src/Entity/Project.php
@@ -125,4 +125,9 @@ class Project
 
         return $this;
     }
+
+    public function __toString(): string
+    {
+        return $this->name;
+    }
 }
--- src/Entity/Technology.php
+++ src/Entity/Technology.php
@@ -72,4 +72,9 @@ class Technology
 
         return $this;
     }
+
+    public function __toString(): string
+    {
+        return $this->name;
+    }
 }
