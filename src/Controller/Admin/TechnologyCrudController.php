151db7c93314dacca6eec4e418fbf0e54ee94bea
Michał Stancelewski <michal@stancelewski.pl>
2022-04-13 23:37:56 +0200

    Update TechnologyCrudController.php

--- src/Controller/Admin/TechnologyCrudController.php
+++ src/Controller/Admin/TechnologyCrudController.php
@@ -3,7 +3,13 @@
 namespace App\Controller\Admin;
 
 use App\Entity\Technology;
+use EasyCorp\Bundle\EasyAdminBundle\Config\Crud;
+use EasyCorp\Bundle\EasyAdminBundle\Config\Filters;
 use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
+use EasyCorp\Bundle\EasyAdminBundle\Field\AssociationField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\TextareaField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\TextField;
+use EasyCorp\Bundle\EasyAdminBundle\Filter\EntityFilter;
 
 class TechnologyCrudController extends AbstractCrudController
 {
@@ -12,14 +18,27 @@ class TechnologyCrudController extends AbstractCrudController
         return Technology::class;
     }
 
-    /*
+    public function configureCrud(Crud $crud): Crud
+    {
+        return $crud
+            ->setEntityLabelInSingular('Technology')
+            ->setEntityLabelInPlural('Technologies')
+            ->setSearchFields(['name', 'description'])
+            ->setDefaultSort(['name' => 'DESC']);
+    }
+
+    public function configureFilters(Filters $filters): Filters
+    {
+        return $filters
+            ->add(EntityFilter::new('project'));
+    }
+
     public function configureFields(string $pageName): iterable
     {
-        return [
-            IdField::new('id'),
-            TextField::new('title'),
-            TextEditorField::new('description'),
-        ];
+        yield TextField::new('name');
+        yield AssociationField::new('projects');
+        yield TextareaField::new('description')
+            ->hideOnIndex();
     }
-    */
+
 }
