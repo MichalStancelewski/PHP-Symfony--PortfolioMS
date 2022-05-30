cf8014151f950a8a26efcfc2a79452d6317221eb
Michał Stancelewski <michal@stancelewski.pl>
2022-05-30 19:36:14 +0200

    Added basic Single project template

--- src/Controller/ProjectController.php
+++ src/Controller/ProjectController.php
@@ -2,7 +2,9 @@
 
 namespace App\Controller;
 
+use App\Entity\Project;
 use App\Repository\ProjectRepository;
+use App\Repository\TechnologyRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -11,10 +13,20 @@ use Twig\Environment;
 class ProjectController extends AbstractController
 {
     #[Route('/', name: 'homepage')]
-    public function index(Environment $twig, ProjectRepository $projectRepository): Response
+    public function index(Environment $twig, ProjectRepository $projectRepository, TechnologyRepository $technologyRepository): Response
     {
         return new Response($twig->render('project/index.html.twig', [
             'projects' => $projectRepository->findAll(),
+            'technologies' => $technologyRepository->findAll(),
         ]));
     }
+
+    #[Route('/project/{id}', name: 'project')]
+    public function single(Environment $twig, Project $project, TechnologyRepository $technologyRepository): Response
+    {
+        return new Response($twig->render('project/single.html.twig', [
+            'project' => $project,
+        ]));
+    }
+
 }
