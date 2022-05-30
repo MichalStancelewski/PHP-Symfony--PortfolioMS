4d6c3583cf0adaa2af16cbaa8fd4d13b58616a9f
Michał Stancelewski <michal@stancelewski.pl>
2022-05-30 16:58:19 +0200

    Changed default ProjectControler template

--- src/Controller/ProjectController.php
+++ src/Controller/ProjectController.php
@@ -2,17 +2,19 @@
 
 namespace App\Controller;
 
+use App\Repository\ProjectRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Twig\Environment;
 
 class ProjectController extends AbstractController
 {
     #[Route('/', name: 'homepage')]
-    public function index(): Response
+    public function index(Environment $twig, ProjectRepository $projectRepository): Response
     {
-        return $this->render('project/index.html.twig', [
-            'controller_name' => 'ProjectController',
-        ]);
+        return new Response($twig->render('project/index.html.twig', [
+            'projects' => $projectRepository->findAll(),
+        ]));
     }
 }
